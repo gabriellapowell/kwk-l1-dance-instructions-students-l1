d95d68a36d82cc8782c40e88cadb3d00e994889d
gabriellapowell <soccerplayer60@icloud.com>
2018-06-04 22:37:01 +0000

    automatically backed up by learn

---
+++ about_me.rb
@@ -0,0 +1,7 @@
+local_greeting = "Salutations!"
+your_name = "Randy"
+my_name = "Frank"
+time_of_day= "Summer"
+
+def local_greeting
+  puts "#{local_greeting}
--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,39 @@
+def lower_body
+  puts "Assuming the proper stance" 
+  "Plant legs far apart, bend knees slightly and keep posture loose"
+ puts "Base footwork." 
+ puts "Step 1: Lift right foot."
+puts "Step 2: Return right foot to the ground."
+puts "Step 3: Finishing with a small skip-step backward."
+end 
 
-# Code your methods below
+def skip_step_left
+  puts "Skip Step Left"
+  puts "Step 1:Lower left foot to the ground"
+puts "Step 2: Bounce left foot back up slightly, kicking it a few inches back."
+puts "Left, Right, Left and Left."
+end 
+
+def skip_step_right
+  puts "Skip Step Right"
+  puts "Step 1:Lower Right foot to the ground"
+puts "Step 2: Bounce right foot back up slightly, kicking it a few inches back."
+
+def upper_body1
+  puts "Upper Body"
+  puts "Bob the Reins"
+  puts "Holding your arms out in front of you, straight at chest level."
+  puts "Cross your right wrist over your left and hold them together."
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+end 
+
+def upper_body2
+  puts "Lasso"
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+puts "Lift right arm up and point right elbow diagonally to the right."
+puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+end
+
+
+puts "Have you ever wondered how to do the Gangdam Style dance? Here are the steps to learning the dance. Start by #{lower_body}.Then #{skip_step_left} followed by #{skip_step_right}. You then want to use your #{upper_body1} and #{upper_body2}.Remember to Bob the reins eight times, WHILE ALSO stepping right, left, right, right, then left, right, left, left, all at the same time. Your arm movements and s
+  
